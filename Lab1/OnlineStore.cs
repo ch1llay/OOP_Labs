405ecf5808f7405a177868ef732f91bda359000c
ch1llay <ch1llay@yandex.ru>
2022-02-15 08:24:27 +0300

    Добавьте файлы проекта.

---
+++ Lab1/OnlineStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class OnlineStore
+    {
+        public string address;
+        public string title;
+        public int amountSoldProducts;
+        public int amountKindProducts;
+        public decimal currentProfit;
+        public int amountOlineUsers;
+        public int amountRegUsers;
+
+        public OnlineStore() { }
+        public OnlineStore(string title)
+        {
+            this.title = title;
+        }
+
+        public OnlineStore(string address, string title, int amountSoldProducts, int amountProducts, decimal currentProfit, int amountOlineUsers, int amountRegUsers) : this(address)
+        {
+            this.title = title;
+            this.amountSoldProducts = amountSoldProducts;
+            this.amountKindProducts = amountProducts;
+            this.currentProfit = currentProfit;
+            this.amountOlineUsers = amountOlineUsers;
+            this.amountRegUsers = amountRegUsers;
+        }
+
+        public override string ToString()
+        {
+            return $"Название интернет магазина {title}\n" +
+                $"Адрес интернет магазина {address}\n" +
+                $"Количество проданных товаров {amountSoldProducts}\n" +
+                $"Количество различных товаров на сайте {amountKindProducts}\n" +
+                $"Текущая прибыль {currentProfit}\n" +
+                $"Текущий онлайн пользователей {amountOlineUsers}\n" +
+                $"Количество зарегистрированных пользователей {amountOlineUsers}";
+        }
+        public void ShowAmoountOnline()
+        {
+            Console.WriteLine($"Текущий онлайн пользователей {amountOlineUsers}");
+        }
+
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlineStore onlineStore = new OnlineStore("BuyPhones.ru", "Наушники на любой вкус", 131, 100, 123401.012m, 22, 15);
+
+            Console.WriteLine("Приветствую. Лабораторная работа № 1. Класс. Создание объекта. Работа с консолью.\n" +
+                "Варинт 11 Интернет магазин. Чапаев Илья");
+            Console.WriteLine($"Вывод поля - Название интренет магазина {onlineStore.title}");
+            Console.WriteLine("Метод который реализует вывод одного поля");
+            onlineStore.ShowAmoountOnline();
+            Console.WriteLine("Вывод объекта через перегрузку ToString()");
+            Console.WriteLine(onlineStore);
+            Console.WriteLine("Вывод параметра в 16 ричном виде");
+            Console.WriteLine(onlineStore.amountRegUsers.ToString("X2"));
+        }
+    }
+}
