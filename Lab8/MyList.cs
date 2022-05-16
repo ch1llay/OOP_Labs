ec54d11659f186eb7c62f8537059854e9cd565fd
ch1llay <ch1llay@yandex.ru>
2022-05-17 01:15:23 +0300

    AddMyList

---
+++ Lab8/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class MyList<T> : IDisposable where T : class
+    {
+        public List<T> Items { get; set; }
+        public int Count { get; set; }
+        public MyList()
+        {
+            Items = new List<T>();
+            Count = 0;
+        }
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+        public void Delete(int index)
+        {
+            Items.RemoveAt(index);
+        }
+        public void Dispose()
+        {
+            Items.Clear();
+        }
+        ~MyList()
+        {
+            Items.Clear();
+        }
+    }
+}
